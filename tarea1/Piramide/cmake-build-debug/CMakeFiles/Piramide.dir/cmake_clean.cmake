20ef4d6c938b04b1066d2d95ec4ee2232a152e6c
Christian <chris.rivera9582@gmail.com>
2019-04-09 19:29:48 -0600

    Algoritmos

---
+++ tarea1/Piramide/cmake-build-debug/CMakeFiles/Piramide.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Piramide.dir/main.cpp.o"
+  "Piramide.pdb"
+  "Piramide"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Piramide.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ tarea1/Primo/cmake-build-debug/CMakeFiles/Tarea1.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Tarea1.dir/main.cpp.o"
+  "Tarea1.pdb"
+  "Tarea1"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Tarea1.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
