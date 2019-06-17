0b54a5b8e42cfb0079b78c43783f502c3e8ab4f2
Christian <chris.rivera9582@gmail.com>
2019-06-17 15:33:55 -0600

    Create Hash main

--- HashTable/main.cpp
+++ HashTable/main.cpp
@@ -1,6 +1,84 @@
 #include <iostream>
 
-int main() {
-    std::cout << "Hello, World!" << std::endl;
-    return 0;
+
+int main()
+{
+
+    BST t;
+    bool exit = false;
+
+    while(!exit){
+        cout << "1. Add Leaf" << endl;
+        cout << "2. FindSmallest" << endl;
+        cout << "3. Find Biggest" << endl;
+        cout << "4. Print InOrder" << endl;
+        cout << "5. Print PreOrder" << endl;
+        cout << "6. Print PostOrder" << endl;
+        cout << "7. Print Children" << endl;
+        cout << "8. Remove Node" << endl;
+        cout << "9. Exit" << endl;
+        cout << endl;
+        int option = 0;
+        cout << ">>> ";
+        cin >> option;
+        switch(option){
+            case 1:{
+
+                int key;
+                cout << "Type key value >>> ";
+                cin >> key;
+                t.addLeaf(key);
+                continue;
+            }
+            case 2:{
+
+                cout <<"Smallest value is:  "<<t.FindSmallest()<<endl;
+                continue;
+            }
+            case 3:{
+
+                cout <<"Biggest value is:  "<< t.FindBiggest()<<endl;
+                continue;
+            }
+            case 4:{
+
+                t.PrintInOrder();
+                cout<<endl;
+                cout<<endl;
+                continue;
+            }
+            case 5:{
+
+                t.PreOrder();
+                cout<<endl;
+                cout<<endl;
+                continue;
+            }
+            case 6:{
+                t.PostOrder();
+                cout<<endl;
+                cout<<endl;
+                continue;
+            }
+            case 7:{
+                int key;
+                cout << "Type key value >>> ";
+                cin >> key;
+                t.PrintChildren(key);
+                continue;
+            }
+            case 8:{
+                int key;
+                cout << "Type key value to delete >>> ";
+                cin >> key;
+                t.RemoveNode(key);
+                continue;
+            }
+            case 9:{
+                exit = true;
+                continue;
+            }
+        }
+    }
+
 }
--- PracticeHash/main.cpp
+++ PracticeHash/main.cpp
@@ -6,26 +6,64 @@
 
 using namespace std;
 
-int main(){
+int main()
+{
 
-    Receipt *a = new Receipt(250, "Erasmo", 100.00);
-    Receipt *b = new Receipt(1, "Isaac", 100.00);
-    Receipt *c = new Receipt(3, "Christian", 100.00);
-    Receipt *d = new Receipt(455, "Hector", 100.00);
-    Receipt *e = new Receipt(223, "Leah", 100.00);
-    Receipt *f = new Receipt(545, "Roberto", 100.00);
     Hash h;
+    bool exit = false;
 
-    h.addItem(a);
-    h.addItem(b);
-    h.addItem(c);
-    h.addItem(d);
-    h.addItem(e);
-    h.addItem(f);
-    h.printHashtable();
-    h.printBucketItems(3);
+    while(!exit){
+        cout << "1. Add Item" << endl;
+        cout << "2. Print Hash Table" << endl;
+        cout << "3. Print Bucket of Items" << endl;
+        cout << "4. Remove Item" << endl;
+        cout << "5. Exit" << endl;
+        cout << endl;
+        int option = 0;
+        cout << ">>> ";
+        cin >> option;
+        switch(option){
+            case 1:{
 
+                int invoice;
+                string name;
+                double amount;
+                cout << "Type Receipt Number >>> ";
+                cin >> invoice;
+                cout << "Type Client Name >>> ";
+                cin >> name;
+                cout << "Type Receipt Amount >>> ";
+                cin >> amount;
+                Receipt *item = new Receipt(invoice, name, amount);
+                h.addItem(item);
+                continue;
+            }
+            case 2:{
 
-    return 0;
+                h.printHashtable();
+                continue;
+            }
+            case 3:{
+
+                int index;
+                cout << "Type Index to print  >>> ";
+                cin >> index;
+                h.printBucketItems(index);
+                continue;
+            }
+            case 4:{
+
+                int number;
+                cout << "Type Receipt number to remove  >>> ";
+                cin >> number;
+                h.removeItem(number);
+                continue;
+            }
+            case 5:{
+                exit = true;
+                continue;
+            }
+        }
+    }
 
 }
